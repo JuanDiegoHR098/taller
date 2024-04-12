de071265e2197aea272725c4fc96e8ae8345886f
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-04-12 16:23:08 -0500

    Segundo punto: Disminucion de la dimension cortando una de las dimensiones

--- taller.py
+++ taller.py
@@ -7,3 +7,10 @@ import scipy.io as sio
 #1-)Crear una matriz de Numpy aleatoria de 4 dimensiones y un size de 1200000 
 
 y= np.random.random((30,40,10,100))
+
+
+#2-) Crea una copia de la matriz creada en el ítem anterior (usar método copy) de solo 3 
+# dimensiones (“Cortando una de las dimensiones”) 
+
+q= y[:,:,:,10].copy
+print(q.ndim)
