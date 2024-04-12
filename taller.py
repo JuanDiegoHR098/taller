5b2c4f944f2c81090864ea1a13dfa85782436de7
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-04-12 16:55:46 -0500

    4to punto: Cambiar de forma de la matriz 3D a 2D

--- taller.py
+++ taller.py
@@ -12,8 +12,8 @@ y= np.random.random((30,40,10,100))
 #2-) Crea una copia de la matriz creada en el ítem anterior (usar método copy) de solo 3 
 # dimensiones (“Cortando una de las dimensiones”) 
 
-q= y[:,:,:,10].copy
-print(q.ndim)
+q= y[:,:,:,10].copy()
+print()
 
 
 # 3. De la matriz 3D, muestra todos los atributos propios de dicha matriz , dimensión, tamaño, 
@@ -23,3 +23,10 @@ w=q.shape
 e=q.size
 r= q.dtype
 t= q.ndim
+
+
+#4.) Modificar su forma y pasarla a 2D
+
+u= q.reshape(40,300)
+print(u)
+
