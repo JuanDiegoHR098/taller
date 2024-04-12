5571c1b905cc7a5d4b3ff82113edeecece6ad740
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-04-12 17:39:05 -0500

    Funcion para pasar martiz a dataframe y creacion de funciones suma, multipl, division, promedio y desviacion estandar

--- taller.py
+++ taller.py
@@ -30,3 +30,71 @@ t= q.ndim
 u= q.reshape(40,300)
 print(u)
 
+#5.)Crea una función que reciba la matriz anterior y la pase a un objeto tipo dataframe de Pandas
+
+def dataframe(x):
+    return pd.DataFrame(x)
+
+#6.)Crear una función que permita cargar un archivo .mat y .csv  
+
+def cargar_archivo():
+    print("1.Cargar archivo .mat\n2.Cargar archivo.csv")
+    entrada=int(input("Ingrese una opcion:"))
+    if entrada == 1:
+        dircc= input("Ingrese la direccion del archivo:")
+        return sio.loadmat(dircc)
+    elif entrada ==2:
+        dircc= input("Ingrese la direccion del archivo:")
+        return pd.read_csv(dircc)
+
+
+#7.Cear funciones de suma, resta, multiplicación, división,logaritmo ,promedio, desviación estándar NOTA:Estas funciones deben permitir hacer estos procesos a lo largo de un eje (usando Numpy) 
+
+def suma(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la suma:"))
+            return np.add(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer la suma en ese eje, ingrese un eje valido")
+
+def resta(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la resta:"))
+            return np.subtract(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer la resta en ese eje, ingrese un eje valido")
+
+def division(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la division:"))
+            return np.divide(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer la division en ese eje, ingrese un eje valido")
+
+def multiplicacion(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la multiplicacion:"))
+            return np.multiply(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer la multiplicacion en ese eje, ingrese un eje valido")
+
+def promedio(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la multiplicacion:"))
+            return np.mean(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer el promedio en ese eje, ingrese un eje valido")
+
+def desviacion_estandar(x):
+    while True:
+        try:
+            eje= int(input("Ingresa en que eje desea hacer la multiplicacion:"))
+            return np.nanstd(x,axis=eje)
+        except ValueError:
+            print("Error: No se puede hacer la desviacion en ese eje, ingrese un eje valido")
+
