4bbbf743e7b42559d777ae806cd26a3073f9b8b7
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-04-12 16:16:56 -0500

    Inicializacion de repositorios y primer punto del taller

---
+++ taller.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.io as sio
+
+
+#1-)Crear una matriz de Numpy aleatoria de 4 dimensiones y un size de 1200000 
+
+y= np.random.random((30,40,10,100))
